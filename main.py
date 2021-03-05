5f1070f1de6c080a95067c151b8956d04ad2d88b
10912141 <79698689+10912141@users.noreply.github.com>
2021-03-05 10:51:36 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from myMath import myStatistics
+
+A = float(input("輸入第一個數"))
+B = float(input("輸入第二個數"))
+C = float(input("輸入第三個數"))
+D = float(input("輸入第四個數"))
+E = float(input("輸入第五個數"))
+
+x = [A,B,C,D,E]
+
+a = print("平均值為:", myStatistics.myMean(x))
+
